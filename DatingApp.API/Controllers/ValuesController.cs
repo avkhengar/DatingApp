87e5650d214f6e1feb92bcd7960a1a0db3399eda
avkhengar <avkhengar@gmail.com>
2020-01-27 03:28:54 +0530

    Dating app initialize

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using DatingApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    //POST http://localhost5000/api/values/5
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context) => _context = context;
+
+        //Get api/values/5
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values=await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+        //Get api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value=await _context.Values.FirstOrDefaultAsync(x=>x.Id == id);
+            return Ok(value);
+        }
+    }
+}
